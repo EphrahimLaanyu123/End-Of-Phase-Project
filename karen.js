d3c2131c8a66be4456ebad9b5b41dbd9b75242da
user <ephrahim.lenaiayasa@student.moringaschool.com>
2023-07-08 03:49:57 +0300

    Added a form page where one can submit their house

--- karen.js
+++ karen.js
@@ -36,7 +36,7 @@
 //   laviDiv.style.display = "block";
 // });
 
-////////////////////////////////////////////////////////////////////KILELESHWA////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////KILELESHWA////////////////////////////////////////////////////////////////////////////////////
 function fetchAndDisplayHousesInKileleshwa() {
     const kilediv = document.getElementById("kilediv");
   
@@ -260,31 +260,178 @@ function fetchAndDisplayHousesInKileleshwa() {
   
 
   fetchAndDisplayHousesInLavington();
-  //////////////////////////////////////////////////////////////////////DONE//////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////DONE//////////////////////////////////////////////////////////////////////////////////////
 
-function addHoverEffectAndUnderlineStyling(button) {
-    button.addEventListener("mouseover", () => {
-      button.style.transform = "scale(1.1)";
-      button.style.borderBottom = "2px solid black";
-    });
+
+// Get the Kileleshwa link element
+const kileleshwaLink = document.getElementById("kileleshwa");
+
+// Add a click event listener to the Kileleshwa link
+kileleshwaLink.addEventListener("click", () => {
+  // Hide other divs
+  document.getElementById("karenDiv").style.display = "none";
+  document.getElementById("westieDiv").style.display = "none";
+  document.getElementById("laviDiv").style.display = "none";
+
+  // Display the Kileleshwa div
+  document.getElementById("kilediv").style.display = "block";
+
+  // Fetch and display houses in Kileleshwa
+  fetchAndDisplayHousesInKileleshwa();
+});
+
+// Similarly, add click event listeners for other locations
+const karenLink = document.getElementById("karen");
+karenLink.addEventListener("click", () => {
+  document.getElementById("kilediv").style.display = "none";
+  document.getElementById("westieDiv").style.display = "none";
+  document.getElementById("laviDiv").style.display = "none";
+  document.getElementById("karenDiv").style.display = "block";
+  fetchAndDisplayHousesInKaren();
+});
+
+const westlandsLink = document.getElementById("westlands");
+westlandsLink.addEventListener("click", () => {
+  document.getElementById("kilediv").style.display = "none";
+  document.getElementById("karenDiv").style.display = "none";
+  document.getElementById("laviDiv").style.display = "none";
+  document.getElementById("westieDiv").style.display = "block";
+  fetchAndDisplayHousesInWestlands();
+});
+
+const lavingtonLink = document.getElementById("lavington");
+lavingtonLink.addEventListener("click", () => {
+  document.getElementById("kilediv").style.display = "none";
+  document.getElementById("karenDiv").style.display = "none";
+  document.getElementById("westieDiv").style.display = "none";
+  document.getElementById("laviDiv").style.display = "block";
+  fetchAndDisplayHousesInLavington();
+});
+
+// Hide the div elements initially
+document.getElementById("kilediv").style.display = "none";
+document.getElementById("karenDiv").style.display = "none";
+document.getElementById("westieDiv").style.display = "none";
+document.getElementById("laviDiv").style.display = "none";
+
+// Function to fetch and display all houses
+function fetchAndDisplayHouses() {
+    fetch("http://localhost:3000/properties")
+      .then(response => response.json())
+      .then(data => {
+        const kilediv = document.getElementById("kilediv");
+        const karendiv = document.getElementById("karenDiv");
+        const westieDiv = document.getElementById("westieDiv");
+        const laviDiv = document.getElementById("laviDiv");
   
-    button.addEventListener("mouseout", () => {
-      button.style.transform = "scale(1)";
-      button.style.borderBottom = "none";
-    });
+        // Clear previous content
+        kilediv.innerHTML = "";
+        karendiv.innerHTML = "";
+        westieDiv.innerHTML = "";
+        laviDiv.innerHTML = "";
+  
+        // Iterate through the houses and display their details in styled boxes
+        data.forEach(house => {
+          // Create a box element for each house
+          const houseBox = document.createElement("div");
+          houseBox.classList.add("house-box");
+  
+          // Create an image element for the house
+          const imageElement = document.createElement("img");
+          imageElement.src = house.image;
+          imageElement.alt = house.name;
+          imageElement.classList.add("house-image");
+  
+          // Create individual elements for other house details
+          const nameElement = document.createElement("p");
+          nameElement.textContent = `Name: ${house.name}`;
+  
+          const bedroomsElement = document.createElement("p");
+          bedroomsElement.textContent = `Bedrooms: ${house.bedrooms}`;
+  
+          const bathroomsElement = document.createElement("p");
+          bathroomsElement.textContent = `Bathrooms: ${house.bathrooms}`;
+  
+          const priceElement = document.createElement("p");
+          priceElement.textContent = `Price: ${house.price}`;
+  
+          // Append the elements to the house box
+          houseBox.appendChild(imageElement);
+          houseBox.appendChild(nameElement);
+          houseBox.appendChild(bedroomsElement);
+          houseBox.appendChild(bathroomsElement);
+          houseBox.appendChild(priceElement);
+  
+          // Append the house box to the respective location div based on house location
+          if (house.location === "Kileleshwa") {
+            kilediv.appendChild(houseBox);
+          } else if (house.location === "Karen") {
+            karendiv.appendChild(houseBox);
+          } else if (house.location === "Westlands") {
+            westieDiv.appendChild(houseBox);
+          } else if (house.location === "Lavington") {
+            laviDiv.appendChild(houseBox);
+          }
+        });
+      })
+      .catch(error => {
+        console.log("Error fetching houses:", error);
+      });
   }
   
-  const homeButton = document.getElementById("TheHomeBar");
-  const locationsButton = document.getElementById("HomeLocations");
-  const servicesButton = document.getElementById("services");
-  const aboutButton = document.getElementById("about");
-  const contactsButton = document.getElementById("contacts");
+  // Function to handle the form submission
+  function handleFormSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
   
-  addHoverEffectAndUnderlineStyling(homeButton);
-  addHoverEffectAndUnderlineStyling(locationsButton);
-  addHoverEffectAndUnderlineStyling(servicesButton);
-  addHoverEffectAndUnderlineStyling(aboutButton);
-  addHoverEffectAndUnderlineStyling(contactsButton);
-
+    // Get the form input values
+    const name = document.getElementById("nameInput").value;
+    const location = document.getElementById("locationInput").value;
+    const bedrooms = document.getElementById("bedroomsInput").value;
+    const bathrooms = document.getElementById("bathroomsInput").value;
+    const price = document.getElementById("priceInput").value;
+    const image = document.getElementById("imageInput").value;
   
+    // Validate the house location
+    const allowedLocations = ["Westlands", "Lavington", "Karen", "Kileleshwa"];
+    if (!allowedLocations.includes(location)) {
+      alert("Invalid house location. Please choose from Westlands, Lavington, Karen, or Kileleshwa.");
+      return;
+    }
+  
+    // Create a house object with the input values
+    const house = {
+      name: name,
+      location: location,
+      bedrooms: bedrooms,
+      bathrooms: bathrooms,
+      price: price,
+      image: image
+    };
+  
+    // Send the POST request to the server
+    fetch("http://localhost:3000/properties", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(house)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("House details posted successfully:", data);
+        // Fetch and display all houses, including the newly added one
+        fetchAndDisplayHouses();
+      })
+      .catch(error => {
+        console.log("Error posting house details:", error);
+        // Optionally, you can display an error message or perform any other error handling
+      });
+  }
+  
+  // Add a submit event listener to the form
+  const form = document.getElementById("houseForm");
+  form.addEventListener("submit", handleFormSubmit);
+  
+  // Fetch and display all houses on page load
+  fetchAndDisplayHouses();
   
